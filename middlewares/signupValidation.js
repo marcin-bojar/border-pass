35f014b245719ee747d8996d98f8476170b8f755
marcin-bojar <marcin.bojar@interia.pl>
2021-01-15 22:32:10 +0100

    signupValidation checks if all fields are filled in

--- middlewares/signupValidation.js
+++ middlewares/signupValidation.js
@@ -6,13 +6,21 @@ const checkIfUserAlreadyExists = (req, res, next) => {
       if (user)
         res.status(400).json({
           success: false,
-          error: 'Podany adres email jest już w użyciu',
+          error: 'Podany adres email jest już w użyciu!',
         });
       else next();
     })
     .catch(err => res.status(400).json({ success: false, error: err.message }));
 };
 
+const checkIfAllFieldsAreFilledIn = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password)
+    res.status(400).json({ success: false, error: 'Wypełnij wszystkie pola!' });
+};
+
 module.exports = signupValidation = {
   checkIfUserAlreadyExists,
+  checkIfAllFieldsAreFilledIn,
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,39 +8,46 @@ const User = require('../../models/User');
 // @route POST /api/users
 // @desc Register new user
 // @public
-router.post('/', signupValidation.checkIfUserAlreadyExists, (req, res) => {
-  const { name, email, password } = req.body;
+router.post(
+  '/',
+  [
+    signupValidation.checkIfUserAlreadyExists,
+    signupValidation.checkIfAllFieldsAreFilledIn,
+  ],
+  (req, res) => {
+    const { name, email, password } = req.body;
 
-  const newUser = new User({
-    name,
-    email,
-    password,
-  });
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
 
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) throw err;
-
-    bcrypt.hash(password, salt, (err, hash) => {
+    bcrypt.genSalt(10, (err, salt) => {
       if (err) throw err;
 
-      newUser.password = hash;
-      newUser
-        .save()
-        .then(user =>
-          res.json({
-            success: true,
-            data: {
-              id: user.id,
-              name: user.name,
-              email: user.email,
-            },
-          })
-        )
-        .catch(err =>
-          res.status(400).json({ success: false, error: err.message })
-        );
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) throw err;
+
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user =>
+            res.json({
+              success: true,
+              data: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+              },
+            })
+          )
+          .catch(err =>
+            res.status(400).json({ success: false, error: err.message })
+          );
+      });
     });
-  });
-});
+  }
+);
 
 module.exports = router;
