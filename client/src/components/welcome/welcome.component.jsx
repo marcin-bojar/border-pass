1e6652dc750532fc5ed66ab89af2b9af1b0eab3c
marcin-bojar <marcin.bojar@interia.pl>
2021-01-30 12:16:31 +0100

    Basic user guide in Welcome component.

--- client/src/components/welcome/welcome.component.jsx
+++ client/src/components/welcome/welcome.component.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import { AppContext } from '../../hooks/useAppState';
 
 import './welcome.styles.scss';
 
-const Welcome = () => (
-  <div className="welcome">
-    <h3 className="welcome__title">Witaj w Border Pass!</h3>
-    <p className="welcome__text">W jakim obecnie znajdujesz się kraju?</p>
-  </div>
-);
+const Welcome = () => {
+  const { currentUser } = useContext(AppContext);
+
+  const noUserContent = (
+    <>
+      <p className="welcome__text">
+        <Link to="/signin" className="welcome__link">
+          Zaloguj się
+        </Link>
+        , aby wczytać swoją historię przekroczeń granic.
+      </p>
+      <p className="welcome__text">
+        <Link to="/signup" className="welcome__link">
+          Zarejestruj się
+        </Link>
+        , aby utworzyć nowe konto.
+      </p>
+      <p className="welcome__text">
+        Możesz również kontynuować jako niezalogowany użytkownik. Pamietaj
+        jednak, że Twoja historia przekroczeń granic będzie w tym przypadku
+        zapisywana w pamięci Twojego urządzenia, co grozi utratą danych.{' '}
+      </p>
+      <p className="welcome__text">
+        Jeśli nie chcesz się logować, określ w jakim obecnie znajdujesz się
+        kraju.
+      </p>
+    </>
+  );
+
+  return (
+    <div className="welcome">
+      <h3 className="welcome__title">Witaj w Border Pass!</h3>
+      {currentUser ? (
+        <p className="welcome__text">W jakim obecnie znajdujesz się kraju?</p>
+      ) : (
+        noUserContent
+      )}
+    </div>
+  );
+};
 
 export default Welcome;
