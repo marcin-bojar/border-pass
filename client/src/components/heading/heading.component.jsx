8b789cf77a7ae2036fa93a51761cb4247a96f23e
marcin-bojar <marcin.bojar@interia.pl>
2021-02-05 22:59:58 +0100

    Heading component serves as link to homepage

--- client/src/components/heading/heading.component.jsx
+++ client/src/components/heading/heading.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Logo from '../logo/logo.component';
 
 import './heading.styles.scss';
 
 const Heading = () => (
-  <div className="heading">
+  <Link to="/" className="heading">
     <Logo />
     <h2 className="heading__subtitle">Rejestruj swoje przekroczenia granic</h2>
-  </div>
+  </Link>
 );
 
 export default Heading;
