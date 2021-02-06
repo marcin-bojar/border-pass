693b7e3a8380ff6104b901e7b1da934465061f9b
marcin-bojar <marcin.bojar@interia.pl>
2021-02-06 13:11:29 +0100

    Not Heading but Logo component serves as link to homepage. Some responsive styles adjusted of Logo and Heading component.

--- client/src/components/heading/heading.component.jsx
+++ client/src/components/heading/heading.component.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Logo from '../logo/logo.component';
 
 import './heading.styles.scss';
 
 const Heading = () => (
-  <Link to="/" className="heading">
+  <div className="heading">
     <Logo />
     <h2 className="heading__subtitle">Rejestruj swoje przekroczenia granic</h2>
-  </Link>
+  </div>
 );
 
 export default Heading;
--- client/src/components/logo/logo.component.jsx
+++ client/src/components/logo/logo.component.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Icon from '../../assets/icons/transfer.svg';
 
 import './logo.styles.scss';
 
 const Logo = () => (
-  <div className="logo">
+  <Link to="/" className="logo">
     <h1 className="logo__title">Border</h1>
     <Icon className="logo__icon" />
     <h1 className="logo__title">Pass</h1>
-  </div>
+  </Link>
 );
 
 export default Logo;
