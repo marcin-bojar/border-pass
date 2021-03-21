27f4932c499269337f75e9572ea47e7f9c58a32c
marcin-bojar <marcin.bojar@interia.pl>
2021-03-21 19:41:59 +0100

    Get route /api/tables added to retrieve all archived tables for the user.

--- routes/api/tables.js
+++ routes/api/tables.js
@@ -20,11 +20,29 @@ router.post('/', [auth, createBordersFile], (req, res) => {
     .then(savedTable => {
       return res.json({ success: true, data: savedTable });
     })
-    .catch(err =>
+    .catch(() =>
       res.status(500).json({
         success: false,
-        error: err,
+        error: 'Archiwizacja nieudana, spróbuj ponownie.',
       })
+    );
+});
+
+// @route GET /api/tables/
+// @desc Post new table
+// @private
+router.get('/', auth, (req, res) => {
+  const { user } = req;
+
+  Table.find({ user: user.id })
+    .then(tables => res.json({ success: true, data: tables }))
+    .catch(err =>
+      res
+        .status(400)
+        .json({
+          success: false,
+          error: 'Nie udało się pobrać danych, spróbuj ponownie.',
+        })
     );
 });
 
