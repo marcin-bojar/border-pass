7875ed56137a521860a4759868d73409903a89ef
marcin-bojar <marcin.bojar@interia.pl>
2021-05-21 23:28:59 +0200

    [FIX] (server.js not staged in previous commit)) express-sslify's HTTPS method must be called before express.static() in order to work.

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use('/api/tables', require('./routes/api/tables'));
 
 //Serve static assets and enforce https in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/dist'));
   app.use(secure.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static('client/dist'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
