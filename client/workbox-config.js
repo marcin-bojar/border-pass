59921f7a6849620714f50378ab53d8e477620d9d
marcin-bojar <marcin.bojar@interia.pl>
2021-05-22 21:38:28 +0200

    [UPDATE] Optimized loading Google fonts.

--- client/workbox-config.js
+++ client/workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   globDirectory: 'dist/',
-  globPatterns: ['**/*.{css,js,html,png,svg,jpg,gif,json}'],
+  globPatterns: ['**/*.{css,js,html,png,svg,jpg,gif,json,woff2}'],
   swDest: 'dist/sw.js',
   mode: 'production',
   cleanupOutdatedCaches: true,
