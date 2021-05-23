25c586689336c44eee676ea63144be34af5ee644
marcin-bojar <marcin.bojar@interia.pl>
2021-05-23 13:42:44 +0200

    [ADD] Runtime caching added to workbox config to cache Google fonts and serve them from service worker.

--- client/workbox-config.js
+++ client/workbox-config.js
@@ -1,7 +1,27 @@
 module.exports = {
   globDirectory: 'dist/',
-  globPatterns: ['**/*.{css,js,html,png,svg,jpg,gif,json,woff2}'],
+  globPatterns: ['**/*.{css,js,html,png,svg,jpg,gif,json}'],
   swDest: 'dist/sw.js',
   mode: 'production',
   cleanupOutdatedCaches: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'google-fonts-stylesheets',
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfonts',
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 180,
+          maxEntries: 30,
+        },
+      },
+    },
+  ],
 };
