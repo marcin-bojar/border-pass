b1f467d16c0a0c839a2653487cf9b92774ca2d2b
marcin-bojar <marcin.bojar@interia.pl>
2021-06-22 19:39:37 +0200

    [CYPRESS] Seeding the DB before login functionality tests. Reseting the DB after those tests.

--- client/cypress/integration/functionalities/auth/loginUser.spec.js
+++ client/cypress/integration/functionalities/auth/loginUser.spec.js
@@ -1,30 +1,40 @@
-describe('Logging in and out', () => {
+describe('Log in functionality', () => {
+  before(() => {
+    cy.exec('npm run seed:db');
+  });
+
   beforeEach(() => {
     cy.visit('/signin');
   });
 
+  after(() => {
+    cy.exec('npm run reset:db');
+  });
+
   it('Logs in with valid data and logs out', () => {
-    cy.loginUser('test@test.pl', 'marcin1');
+    cy.loginUser('testing@test.pl', 'password123');
     cy.checkUserHeader();
-    cy.checkUserName('Test Testowy');
+    cy.checkUserName('Tester Name');
     cy.getLocalStorage('token').should('exist');
     cy.contains('button.navbar', 'Wyloguj').click();
   });
 
   it('Doesnt log in with wrong password', () => {
-    cy.loginUser('test@test.pl', 'wrong');
+    cy.loginUser('testing@test.pl', 'wrong');
     cy.checkErrorMessage('Podane dane logowania są błędne.');
     cy.checkGuestHeader();
   });
 
   it('Doesnt log in without password', () => {
-    cy.get('input[name=email]').type('test@test.pl{enter}').should('have.value', 'test@test.pl');
+    cy.get('input[name=email]')
+      .type('testing@test.pl{enter}')
+      .should('have.value', 'testing@test.pl');
     cy.checkErrorMessage('Podane dane logowania są błędne.');
     cy.checkGuestHeader();
   });
 
   it('Doesnt log in with wrong email', () => {
-    cy.loginUser('wrong@test.pl', 'marcin1');
+    cy.loginUser('wrong@test.pl', 'password123');
     cy.checkErrorMessage('Podany użytkownik nie istnieje.');
     cy.checkGuestHeader();
   });
